4bfd87f6d592582d28f1e849a3e7fc82b721958d
GuilhermeVinicius01 <guilhermevinicius.d.s@gmail.com>
2025-04-22 19:30:37 -0300

    Q2.java

---
+++ q2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class q2 {
+    public static void main(String args[]) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o seu salario:");
+        double salario = Double.valueOf(entrada.nextLine());
+
+        if (salario > 1250){
+        double total = (0.10 * salario);
+        double salario_final = (total + salario);
+        System.out.println("Aumento: " +total);
+        System.out.println("Salario Final: " +salario_final);
+        
+        }else{
+            double total = (0.15 * salario);
+            double salario_final = (total + salario);
+            System.out.println("Aumento: " +total);
+            System.out.println("Salario Final: " +salario_final);}
+        }
+    }
